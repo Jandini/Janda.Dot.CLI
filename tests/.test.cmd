d41bec0e1c83a44d59354947ba33000c11b5017a
Matt <matt.janda.kingston@gmail.com>
2019-10-25 17:59:25 +0100

    Update unit test

--- tests/.test.cmd
+++ tests/.test.cmd
@@ -1,14 +1,16 @@
 @echo off
-cd %TEMP%
+rem call dots to receive few variables
+
 call .dots
 
 
-SET COMMAND=.help
-<nul set /p =Running %COMMAND%	
-call %COMMAND% > %DOT_NUL%
-if %ERRORLEVEL% neq 1 echo [ FAILED ] && exit 1 /b
-echo [ OK ]
+:external
+set TEST_DIR=%TEMP%\T%RANDOM%
+mkdir %TEST_DIR%
+cd %TEST_DIR%
 
+
+call :help_test 
 call :help_test help
 call :help_test addcon
 call :help_test addlib
@@ -22,15 +24,12 @@ call :help_test develop
 call :help_test diff
 call :help_test dotnet
 call :help_test dots
-call :help_test dotsln
 call :help_test feature
 call :help_test foreach
 call :help_test gitlab
 call :help_test init
-call :help_test kawika
 call :help_test master
 call :help_test mirror
-call :help_test net
 call :help_test newsln
 call :help_test origin
 call :help_test pack
@@ -44,17 +43,19 @@ call :help_test version
 call :help_test release
 
 
-goto exit
 
+
+goto exit
 
 
 :help_test
 SET COMMAND=%1
 <nul set /p =Running .help %COMMAND%	
-call .help %COMMAND% > %DOT_NUL%
-set RESULT=%ERRORLEVEL% 
-if %RESULT% neq 1 echo [ FAILED ] && echo Expected value is 1. Return value is %RESULT% && exit 1 /b
+if "%1" equ "" ( call .help > %DOT_NUL% ) else ( call .help %COMMAND% > %DOT_NUL% )
+if %ERRORLEVEL% neq 1 echo [ FAILED ] && echo Expected value is 1. Return value is %ERRORLEVEL% && exit 1 /b
 echo [ OK ]
-:EOF
+exit /b
 
 :exit
+cd %CURRENT_DIR_PATH%
+rd /q %TEST_DIR%
