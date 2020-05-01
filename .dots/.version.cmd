a6064f50e52f1a954c986d5356b2fea3f9461c5e
Matt <matt.janda.kingston@gmail.com>
2020-05-01 11:00:12 +0100

    chore: Remove jq from version command

--- .dots/.version.cmd
+++ .dots/.version.cmd
@@ -3,15 +3,14 @@ if %ERRORLEVEL% equ 1 exit /b
 
 if "%1" equ "" (set VERSION_NAME=InformationalVersion) else (set VERSION_NAME=%1)
 echo Retrieving %VERSION_NAME%...
-set setver=%temp%\_dot%RANDOM%version.cmd 
-gitversion | jq -r "\"set DOT_GIT_VERSION=\"+ .%VERSION_NAME%" > %setver% 2>nul
-if %ERRORLEVEL% neq 0 goto fixed_version
-call %setver%
-del /q %setver%
-goto exit
+call :get_version %VERSION_NAME%
 
-:fixed_version
-set DOT_GIT_VERSION=0.0.0
-
-:exit
 echo %VERSION_NAME% is %DOT_GIT_VERSION%
+goto :eof
+
+
+
+:get_version
+set DOT_GIT_VERSION=0.0.0
+for /f %%i in ('gitversion /showvariable %1') do set DOT_GIT_VERSION=%%i
+goto :eof
