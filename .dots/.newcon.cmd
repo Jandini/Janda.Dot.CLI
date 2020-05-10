e856b690ebe7a5cf329e2a8887c0db7a93ec91f5
Matt <matt.janda.kingston@gmail.com>
2020-05-10 18:32:08 +0100

    Update unit tests

--- .dots/.newcon.cmd
+++ .dots/.newcon.cmd
@@ -1,5 +1,5 @@
 @call _dots %~n0 "Creates new repository and add new console application" "<.|name> [application name]" " G1" %1 %2 %3
-if %ERRORLEVEL% equ 1 exit /b
+if %ERRORLEVEL% equ 1 echo Try use .addcon instead.& exit /b
 
 call .newdot %1
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL% /b
