fac1a261ef361c1a25e249347638255397013990
Matt <matt.janda.kingston@gmail.com>
2020-05-23 18:19:40 +0100

    feat: Add dot name parser

---
+++ .dots/_dotname.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+set INPUT_NAME=%~1
+set INPUT_VARIABLE=%~2
+set RESULT=%INPUT_NAME%
+
+if "%INPUT_NAME:~0,1%"=="." set RESULT=%DOT_BASE_NAME%.%INPUT_NAME:~1%
+if "%INPUT_NAME%" equ "." set RESULT=%DOT_BASE_NAME%
+
+set %INPUT_VARIABLE%=%RESULT%
+
