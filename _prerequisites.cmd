d17e8989f701f1c1933a391543b8205479f74154
Matt Janda <matt.janda.kingston@gmail.com>
2020-05-27 09:24:35 +0100

    chore: Add choco configure setting

--- _prerequisites.cmd
+++ _prerequisites.cmd
@@ -28,6 +28,7 @@ title Installing choco...
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 :configure
 choco feature enable -n allowGlobalConfirmation > nul
+choco feature enable -n exitOnRebootDetected > nul
 goto :eof
 
 
