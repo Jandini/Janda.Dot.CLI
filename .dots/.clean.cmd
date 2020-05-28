e1a0a8a7ee7784a750072250e0d5a9d999149239
Matt <matt.janda.kingston@gmail.com>
2020-05-28 22:58:48 +0100

    feat: Add delete obj and bin dirs to clean command

--- .dots/.clean.cmd
+++ .dots/.clean.cmd
@@ -8,10 +8,21 @@ rem :::
 
 if "%1" equ "*" goto foreach 
 
-.dotnet clean %1
+call .dotnet clean %1
+echo Searching for bin and obj folders...
+for /d /r . %%d in (bin,obj) do call :remove_dir "%%d"
 goto :eof
 
 :foreach
 .foreach dotnet clean %2
+goto :eof
+
+
+
+:remove_dir
+if not exist "%~1" goto :eof 
+echo Deleting %~1
+rd /s/q "%~1"
+goto :eof
 
 
