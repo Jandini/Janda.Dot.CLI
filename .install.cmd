141d5d6ca532fed5996cc4c14432acc389ce9cf8
Matt Janda <matt.janda.kingston@gmail.com>
2020-05-29 23:11:53 +0100

    feat: Add error handling to prerequisites installation

--- .install.cmd
+++ .install.cmd
@@ -5,12 +5,15 @@ call .prerequisites check
 if %ERRORLEVEL% equ 0 goto build
 
 echo Installing prerequisites...
-rem Run 
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath 'cmd.exe' -ArgumentList '/c \"%~dp0\_prerequisites.cmd\"' -Verb runAs"
- 
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "$process = (Start-Process -Wait -PassThru -FilePath 'cmd.exe' -ArgumentList '/c \"%~dp0\.prerequisites.cmd\"' -Verb runAs); exit $process.ExitCode"
+if %ERRORLEVEL% equ 350 echo Computer restart is required to complete prerequisites installation. & exit %ERRORLEVEL%
+if %ERRORLEVEL% neq 0 echo Prerequisites are incomplete & exit %ERRORLEVEL%
+
 :build
 call .pack
 
-echo Installation complete
+echo Installation complete.
 goto :eof
+
+
 
