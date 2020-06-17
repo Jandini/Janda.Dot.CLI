5723f4b51ba4655cfa75aa7a248a32a09b97e08e
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-17 13:25:29 +0100

    chore: Update .branch

--- .dots/.branch.cmd
+++ .dots/.branch.cmd
@@ -1,25 +1,31 @@
-@call _dots %~n0 " g" %1 %2 %3
+@call _dots %~n0 %* --require-git 
 if %ERRORLEVEL% equ 1 exit /b
 
-rem ::: List git branches or checkout selected branch
+rem ::: Show available branches or switch branch
 rem ::: 
-rem ::: .BRANCH [branch-name]
+rem ::: .BRANCH [branch name]
+rem ::: 
+rem ::: Parameters:
+rem :::     branch name - full or partial branch name
+rem ::: 
+rem ::: Description: 
+rem :::     Display list of available branches. The current branch is marked with "*".
+rem :::     Checkout branch based on given branch name. If the branch name is partial,
+rem :::     then first matching branch is checked out.
 rem ::: 
 
-if "%1" neq "" goto checkout_branch
+
+set BRANCH_NAME=%DOT_ARG_DEFAULT%
+if "%BRANCH_NAME%" neq "" goto checkout_branch
 git branch
 goto :eof
 
 :checkout_branch
-echo Checking out %1
-git checkout %1
+echo Checking out %BRANCH_NAME%
+git checkout %BRANCH_NAME%
 if %ERRORLEVEL% equ 1 goto :find_branch
 git branch
 goto :eof
 
 :find_branch
-call .checkout %1
-
-
-
-
+call .checkout %BRANCH_NAME%
