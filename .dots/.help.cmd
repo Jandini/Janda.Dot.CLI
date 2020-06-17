19315bea5e189361ff473f3db9a91e68723f832b
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-17 13:09:31 +0100

    chore: Update .help

--- .dots/.help.cmd
+++ .dots/.help.cmd
@@ -1,9 +1,13 @@
-@call _dots %~n0 "" %1 %2 %3
+@call _dots %~n0 %*
 if %ERRORLEVEL% equ 1 exit /b
 
 rem ::: Show available commands with short description
 rem ::: 
 rem ::: .HELP [command name]
+rem ::: 
+rem ::: Description: 
+rem :::     List available commands with short description. 
+rem :::     When command name is provided display given command's help.
 rem ::: 
 
 
@@ -13,13 +17,10 @@ if "%COMMAND_NAME%" equ "" (call :show_all) else (call _dothelp %COMMAND_NAME%.c
 goto :eof
 
 
-
-
 :show_all
 set DOT_CMD_MASK=.?*.cmd  
 for /f %%f in ('dir /b %DOT_PATH%%DOT_CMD_MASK%') do call _dothelp %%f desc
 goto :eof
-
 
 
 :parse_name
