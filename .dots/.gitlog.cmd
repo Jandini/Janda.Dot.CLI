e2b88c780fe69ec6e3ca10900893a5ee8304778e
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-18 10:35:03 +0100

    chore: Update .gitlog

--- .dots/.gitlog.cmd
+++ .dots/.gitlog.cmd
@@ -1,9 +1,15 @@
-@call _dots %~n0 --require-git %*
+@call _dots %~n0 %* --require-git
 if %ERRORLEVEL% equ 1 exit /b
 
-rem ::: Show git log
+rem ::: Git log
 rem ::: 
 rem ::: .GITLOG [max count]
+rem ::: 
+rem ::: Parameters: 
+rem :::     max count = Number of entries to display. Default value is 20.
+rem ::: 
+rem ::: Description: 
+rem :::     Show git log
 rem ::: 
 
 
