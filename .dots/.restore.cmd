155c42beb72f3238c71426750e99154b9868d3e0
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-18 21:56:21 +0100

    chore: Update .restore

--- .dots/.restore.cmd
+++ .dots/.restore.cmd
@@ -1,18 +1,15 @@
-@call _dots %~n0 "" %1 %2 %3
-if %ERRORLEVEL% equ 1 exit /b
+@_dotnet restore %~1
 
-rem ::: Run (*resursively) dotnet restore for project in .current folder, repo's default solution or all DOT_BUILD_SOLUTIONS defined in %DOT_CONFIG% file
+rem ::: Dotnet restore
 rem ::: 
 rem ::: .RESTORE [*|.]
 rem ::: 
-
-
-if "%1" equ "*" goto foreach 
-
-.dotnet restore %1
-goto :eof
-
-:foreach
-.foreach dotnet restore %2
-
+rem ::: Parameters: 
+rem :::     * - Search and run the command for each dot repository found.
+rem :::     . - Run the command for current directory only.
+rem ::: 
+rem ::: Description: 
+rem :::     Run the command for solution(s) found in dot repository.
+rem :::     For more details see .dotnet --help
+rem ::: 
 
