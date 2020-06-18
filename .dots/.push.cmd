115cde4add8c455b1f94afb42d09ae64e7d50d59
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-18 20:23:15 +0100

    chore: Update .push

--- .dots/.push.cmd
+++ .dots/.push.cmd
@@ -1,4 +1,5 @@
 @call _dots %~n0 %* --require-git
+if %ERRORLEVEL% equ 1 exit /b
 
 rem ::: Git push with tags
 rem ::: 
@@ -9,7 +10,6 @@ rem :::     Run git push with tags.
 rem :::     git push --all --follow-tags
 rem ::: 
 
-if %ERRORLEVEL% equ 1 exit /b
 git push --all --follow-tags
 
 
