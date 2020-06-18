dcfdb472edd9c34edd79b68901a2a4aae1fd2210
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-18 23:48:47 +0100

    chore: Update .addtest

--- .dots/.addtest.cmd
+++ .dots/.addtest.cmd
@@ -1,35 +1,29 @@
-@call _dots %~n0 "d 1" %1 %2 %3
+@call _dots %~n0 %* --require-dot --require-param 
 if %ERRORLEVEL% equ 1 exit /b
 
-rem ::: Add new xUnit test project to existing or new solution
+rem ::: Dots add xunit test
 rem ::: 
-rem ::: .ADDTEST <.|[.]new test project name> [existing or new solution full name]
+rem ::: .ADDTEST <.|[.]project name> [--solution <name>] [--namespace <name>]
+rem ::: 
+rem ::: Parameters: 
+rem :::     project name - New project name
+rem :::     solution name - Existing or new solution
+rem :::     namespace name - Project namespace 
+rem ::: 
+rem ::: Description: 
+rem :::     Add new xunit test 
 rem ::: 
 
-rem the . creates class library project with the same as default solution name or indicates that the name should be added to base name
+rem Create new library name and SOLUTION
+call _dotname "%~1" PROJECT_NAME
 
-rem by default solution name is the current folder
-set SOLUTION_NAME=%DOT_BASE_NAME%
+call _dotsrc
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-rem if new solution is not provided 
-if "%2" neq "" set SOLUTION_NAME=%2
+call _dotnew xunit %PROJECT_NAME% 
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-set SOLUTION_FILE=%SOLUTION_NAME%.sln
+call _dotsln %PROJECT_NAME%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-SET LIBRARY_NAME=%1
-SET LIB_NAME=%LIBRARY_NAME%
-if "%LIBRARY_NAME:~0,1%"=="." set LIB_NAME=%DOT_BASE_NAME%.%LIBRARY_NAME:~1%
-if "%LIBRARY_NAME%" equ "." set LIB_NAME=%DOT_BASE_NAME%
 
-pushd src
-
-dotnet new xunit -n %LIB_NAME%
-
-if not exist %SOLUTION_FILE% echo Creating %SOLUTION_FILE% && dotnet new sln -n %SOLUTION_NAME%
-echo Adding %LIB_NAME% application to %SOLUTION_FILE%
-dotnet sln %SOLUTION_FILE% add %LIB_NAME%
-
-echo Restoring packages for %LIB_NAME%
-dotnet restore %LIB_NAME% --ignore-failed-sources 
-
-popd
