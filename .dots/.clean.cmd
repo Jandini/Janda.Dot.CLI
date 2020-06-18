66686322bdfd57cb4b6d150df4d3282e5db0d471
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-18 21:42:55 +0100

    chore: Update .clean

--- .dots/.clean.cmd
+++ .dots/.clean.cmd
@@ -1,28 +1,15 @@
-@call _dots %~n0 "" %1 %2 %3
-if %ERRORLEVEL% equ 1 exit /b
+@_dotnet clean %~1
 
-rem ::: Run (*resursively) dotnet clean for project in .current folder, repo's default solution or all DOT_BUILD_SOLUTIONS defined in %DOT_CONFIG% file
+rem ::: Dotnet clean
 rem ::: 
 rem ::: .CLEAN [*|.]
 rem ::: 
-
-if "%1" equ "*" goto foreach 
-
-call .dotnet clean %1
-echo Searching for bin and obj folders...
-for /d /r . %%d in (bin,obj) do call :remove_dir "%%d"
-goto :eof
-
-:foreach
-.foreach dotnet clean %2
-goto :eof
-
-
-
-:remove_dir
-if not exist "%~1" goto :eof 
-echo Deleting %~1
-rd /s/q "%~1"
-goto :eof
-
+rem ::: Parameters: 
+rem :::     * - Search and run the command for each dot repository found.
+rem :::     . - Run the command for current directory only.
+rem ::: 
+rem ::: Description: 
+rem :::     Run the command for solution(s) found in dot repository.
+rem :::     For more details see .dotnet --help
+rem ::: 
 
