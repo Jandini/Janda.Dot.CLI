78690a744ee6a6805db48f2c7c358645eb3f2ef0
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-20 20:29:34 +0100

    feat(_dotadd): Add extra parameter support

--- .dots/_dotadd.cmd
+++ .dots/_dotadd.cmd
@@ -1,7 +1,7 @@
 if defined DOT_ARG_NAMESPACE set DOT_NEW_ARGS=--nameSpace %DOT_ARG_NAMESPACE%
 if defined DOT_ARG_ADD-ARGS set DOT_NEW_ARGS=%DOT_NEW_ARGS% --addArgs
 
-echo Running dotnet new %1 -n %2 %DOT_NEW_ARGS%
-dotnet new %1 -n %2 %DOT_NEW_ARGS%
+echo Running dotnet new %1 -n %2 %DOT_NEW_ARGS% %3
+dotnet new %1 -n %2 %DOT_NEW_ARGS% %3
 
 
