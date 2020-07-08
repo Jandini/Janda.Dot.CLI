969479807bafc0544ec9ba62b766bf1f1e5891b5
Matt Janda <matt.janda.kingston@gmail.com>
2020-07-08 11:11:51 +0100

    feat: Add user nuget config update to the installer

--- .install.cmd
+++ .install.cmd
@@ -10,10 +10,20 @@ if %ERRORLEVEL% equ 350 echo Computer restart is required to complete prerequisi
 if %ERRORLEVEL% neq 0 echo Prerequisites are incomplete & exit %ERRORLEVEL%
 
 :build
+
+call :add_nuget_source "%USERPROFILE%\.nuget\local" nuget.local
 call .pack
+
 
 echo Installation complete.
 goto :eof
 
 
+:add_nuget_source
+dotnet nuget list source | find "%~1" >nul
+if %ERRORLEVEL% equ 0 goto :eof
+
+echo Adding %~2 source "%~1"
+dotnet nuget add source "%~1" -n nuget.local
+goto :eof
 
