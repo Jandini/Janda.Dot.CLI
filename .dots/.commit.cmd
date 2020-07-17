8a44f86b083fb8338b1841f11fa9d9a55f797e22
Matt Janda <matt.janda.kingston@gmail.com>
2020-07-17 21:25:11 +0100

    feat: Bump dot packages version to 0.3.0

--- .dots/.commit.cmd
+++ .dots/.commit.cmd
@@ -3,12 +3,13 @@ if %ERRORLEVEL% equ 1 exit /b
 
 rem ::: Git commit
 rem ::: 
-rem ::: .COMMIT <comment> [--prefix <prefix>] [--scope <scope>]
+rem ::: .COMMIT <comment> [--prefix <prefix>] [--scope <scope>] [--force]
 rem ::: 
 rem ::: Parameters:
 rem :::     comment - Comment text
 rem :::     prefix - conventional commit prefix
 rem :::     scope - optional commit scope 
+rem :::     force - skip confirmation
 rem ::: 
 rem ::: Description: 
 rem :::     Display all unstaged changes and prompt user before continue. 
@@ -31,8 +32,11 @@ if defined DOT_ARG_PREFIX call :conventional_commit "%DOT_ARG_DEFAULT%" "%DOT_AR
 echo You are about to stage all and commit "%COMMIT_COMMENT%"
 git status
 
+if defined DOT_ARG_FORCE goto :force
 set /P CONFIRM=Do you want to stage changes and commit "%COMMIT_COMMENT%" to %DOT_GIT_BRANCH% now (Y/[N])?
 if /i "%CONFIRM%" neq "Y" goto :eof
+:force
+
 
 git add .
 git commit -m "%COMMIT_COMMENT%"
