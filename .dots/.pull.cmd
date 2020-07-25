66c8f1e8b167991c5d276f34cd8e6fde8b7203de
Matt Janda <matt.janda.kingston@gmail.com>
2020-07-25 23:02:51 +0100

    feat(.pull): Automatically add branch tracking

--- .dots/.pull.cmd
+++ .dots/.pull.cmd
@@ -10,6 +10,16 @@ rem :::     Run git pull
 rem ::: 
 
 git pull
+if %ERRORLEVEL% equ 0 exit /b 
+
+echo Something is wrong... Trying to add tracking to %DOT_GIT_BRANCH% branch 
+git branch --set-upstream-to=origin/%DOT_GIT_BRANCH% %DOT_GIT_BRANCH% 
+if %ERRORLEVEL% neq 0 echo I am giving up...&exit /b 
+
+echo Trying to pull again... 
+git pull
+if %ERRORLEVEL% neq 0 echo Pull failed.&exit /b 
+
 
 
 
