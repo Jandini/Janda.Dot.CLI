314ab40d00838866aaa6421898a728acd1bb653b
Matt Janda <matt.janda.kingston@gmail.com>
2020-07-26 10:19:17 +0100

    feat(.pull): Add fetch

--- .dots/.pull.cmd
+++ .dots/.pull.cmd
@@ -9,14 +9,22 @@ rem ::: Description:
 rem :::     Run git pull 
 rem ::: 
 
+echo Pulling...
 git pull
 if %ERRORLEVEL% equ 0 exit /b 
 
-echo Something is wrong... Trying to add tracking to %DOT_GIT_BRANCH% branch 
+echo Fetching...
+git fetch
+
+echo Pulling again... 
+git pull
+if %ERRORLEVEL% equ 0 exit /b 
+
+echo Trying to add tracking to %DOT_GIT_BRANCH% branch 
 git branch --set-upstream-to=origin/%DOT_GIT_BRANCH% %DOT_GIT_BRANCH% 
 if %ERRORLEVEL% neq 0 echo I am giving up...&exit /b 
 
-echo Trying to pull again... 
+echo Pulling... 
 git pull
 if %ERRORLEVEL% neq 0 echo Pull failed.&exit /b 
 
