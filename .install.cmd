76c2c736f7ae9acd3e5cf0d3ad8df98b54f29b40
Matt Janda <matt.janda.kingston@gmail.com>
2020-09-27 14:22:47 +0100

    Update .install.cmd

--- .install.cmd
+++ .install.cmd
@@ -10,7 +10,7 @@ echo Installing prerequisites...
 %DOT_POWERSHELL_CMD% "$process = (Start-Process -Wait -PassThru -FilePath 'cmd.exe' -ArgumentList '/c \"%~dp0\.prerequisites.cmd\"' -Verb runAs); exit $process.ExitCode"
 if %ERRORLEVEL% equ 350 echo Computer restart is required to complete prerequisites installation. & exit %ERRORLEVEL%
 if %ERRORLEVEL% neq 0 echo Prerequisites are incomplete. Re-open command prompt and try again. & exit %ERRORLEVEL%
-call %ALLUSERSPROFILE%\chocolatey\bin\RefreshEnv
+call :refresh_env
 :build
 
 call :add_dots_path "%%USERPROFILE%%\.dots"
@@ -18,6 +18,7 @@ call :add_nuget_source "%USERPROFILE%\.nuget\local" nuget.local
 
 git rev-parse --is-inside-work-tree 1>nul 2>nul
 if %ERRORLEVEL% equ 0 (call .pack) else (call .clone)
+call :refresh_env
 echo Installation complete.
 goto :eof
 
@@ -30,12 +31,15 @@ echo Adding %~2 source "%~1"
 dotnet nuget add source "%~1" -n nuget.local
 goto :eof
 
-
 :add_dots_path
 set INSTALL_PATH=%~1
 set PATH | find "%INSTALL_PATH%" > nul
 if %ERRORLEVEL% equ 0 goto :eof
 echo Adding %INSTALL_PATH% to PATH environment
 %DOT_POWERSHELL_CMD% "$path=[Environment]::GetEnvironmentVariable('path', 'user'); if (!$path.contains('%INSTALL_PATH%')) { $path+=';%INSTALL_PATH%'; [Environment]::SetEnvironmentVariable('path', $($path -join ';'), 'user'); }"
-call %ALLUSERSPROFILE%\chocolatey\bin\RefreshEnv
+call :refresh_env
 exit /b %ERRORLEVEL%
+
+:refresh_env
+call %ALLUSERSPROFILE%\chocolatey\bin\RefreshEnv
+goto :eof
