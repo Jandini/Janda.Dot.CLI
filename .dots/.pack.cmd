68863adf529f6dfc4fa65aeb6389ebf13250b8a4
Matt Janda <matt.janda.kingston@gmail.com>
2021-01-24 19:14:27 +0000

    feat: Automatically add and remove dot nuget to projects given in dot config

--- .dots/.pack.cmd
+++ .dots/.pack.cmd
@@ -1,4 +1,4 @@
-@.dotnet pack %*
+@call _dots %~n0 %* --require-dot
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 rem ::: Dotnet pack
@@ -13,5 +13,22 @@ rem :::     Run the command for solution(s) found in dot repository.
 rem :::     For more details see .dotnet --help
 rem ::: 
 
+
+call _dotsrc
+call :DotNuget add %DOT_NUGET_SOURCES%
+call .dotnet pack %*
+call :DotNuget remove
+goto :eof
+
+
+:DotNuget
+for %%R in ("%DOT_NUGET_PROJECTS:;=" "%") do if "%%R" neq "" call :DotNugetAddRemove %1 %%R %2
+goto :eof
+
+:DotNugetAddRemove
+echo Performing %1 package Janda.Dot.Nuget for project %2...
+dotnet %1 %2 package Janda.Dot.Nuget %3
+if %ERRORLEVEL% neq 0 exit 
+goto :eof
 
 
